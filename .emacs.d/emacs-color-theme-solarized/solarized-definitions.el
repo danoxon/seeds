9e6e6c5699fd6e3e0c6da564110b2053bd989298
Dan Barrett <dbarrett71@gmail.com>
2013-01-15 17:06:58 -0500

    another update

--- .emacs.d/emacs-color-theme-solarized/solarized-definitions.el
+++ .emacs.d/emacs-color-theme-solarized/solarized-definitions.el
@@ -1,4 +1,4 @@
-(eval-when-compile
+2(eval-when-compile
   (require 'cl))
 
 (defconst solarized-description
@@ -60,7 +60,7 @@ the \"Gen RGB\" column in solarized-definitions.el to improve them further."
     (base02  "#073642" "#0a2832" "#262626" "black"         "black")
     (base01  "#586e75" "#465a61" "#585858" "brightgreen"   "green")
     (base00  "#657b83" "#52676f" "#626262" "brightyellow"  "yellow")
-    (base0   "#839496" "#708183" "#808080" "brightblue"    "blue")
+    (base0   "#a8b4b5" "#708183" "#808080" "brightblue"    "blue")
     (base1   "#93a1a1" "#81908f" "#8a8a8a" "brightcyan"    "cyan")
     (base2   "#eee8d5" "#e9e2cb" "#e4e4e4" "white"         "white")
     (base3   "#fdf6e3" "#fcf4dc" "#ffffd7" "brightwhite"   "white")
